fd4e66ed125db97a19c242a521b297efd1d62c6c
Niklas Fähnrich <niklasfahnrich@hotmail.com>
2024-08-02 12:01:33 +0200

    lifted the state to the form component and defined value props to the input fields

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,15 +1,74 @@
-import { ReactElement } from "react";
+import { FormEventHandler, ReactElement, useState } from "react";
 import { Button, Input } from ".";
 
+interface IRegistrationData {
+  name: string;
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
 export function Form(): ReactElement {
+  // ############### Hooks & State ###############
+  // It also works if you want to create five useState variabels, on for each input. But I chose this approach  with an object instead.
+  const [registrationData, setRegistrationData] = useState<IRegistrationData>({
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  // ############### Event Handlers ###############
+
+  const handleOnSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    console.log("Submitted");
+  };
+
+  // ############### Functions ###############
+  const updateRegistrationData = (id: string, value: string) => {
+    // The [id] is using array brackets in order to dynamically set the key corresponding to the id value.
+    setRegistrationData({ ...registrationData, [id]: value });
+  };
+
   return (
-    <form className="form">
-      <Input id="name" label="Name" />
-      <Input id="username" label="Username" />
-      <Input id="email" label="Email" type="email" />
-      <Input id="password" label="Password" type="password" />
-      <Input id="confirm-password" label="Confirm Password" type="password" />
-      <Button disabled type="submit">Sign Up</Button>
+    <form className="form" onSubmit={handleOnSubmit}>
+      <Input
+        id="name"
+        label="Name"
+        updateValue={updateRegistrationData}
+        value={registrationData.name}
+      />
+      <Input
+        id="username"
+        label="Username"
+        updateValue={updateRegistrationData}
+        value={registrationData.username}
+      />
+      <Input
+        id="email"
+        label="Email"
+        type="email"
+        updateValue={updateRegistrationData}
+        value={registrationData.email}
+      />
+      <Input
+        id="password"
+        label="Password"
+        type="password"
+        updateValue={updateRegistrationData}
+        value={registrationData.password}
+      />
+      <Input
+        id="confirmPassword"
+        label="Confirm Password"
+        type="password"
+        updateValue={updateRegistrationData}
+        value={registrationData.confirmPassword}
+      />
+      <Button type="submit">Sign Up</Button>
     </form>
   );
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,21 +6,21 @@ interface IInputProps {
   id?: string;
   label: string;
   type?: TInputType;
+  updateValue: (id: string, value: string) => void;
+  value: string;
 }
 
 export function Input(props: IInputProps): ReactElement {
   // ############### Hooks & State ###############
 
   // For the component to handle state we need to use "useState" from react. It's a hook that react uses to make state persist across rerenders, and it's also used to trigger a rerender of the component every time the state updates. Observe though, if the state is a reference variable, you  always need to create a new reference in order for react to actually trigger a rerender.
-
-  const [value, setValue] = useState<string>("");
   const [labelClasses, setLabelClasses] = useState<string[]>(["label"]);
   const inputRef = useRef<HTMLInputElement>(null);
 
   // ############### Event Handlers ###############
 
   const handleOnBlur: FocusEventHandler<HTMLInputElement> = () => {
-    if (value === "") {
+    if (props.value === "") {
       const updatedClasses = labelClasses.filter(
         (labelClass) => labelClass !== "minimize" && labelClass !== "blue"
       );
@@ -61,11 +61,11 @@ export function Input(props: IInputProps): ReactElement {
         className={derivedInputClasses}
         id={id}
         onBlur={handleOnBlur}
-        onChange={(e) => setValue(e.target.value)}
+        onChange={(e) => props.updateValue(id, e.target.value)}
         onFocus={handleOnFocus}
         ref={inputRef}
         type={type}
-        value={value}
+        value={props.value}
       />
     </div>
   );
